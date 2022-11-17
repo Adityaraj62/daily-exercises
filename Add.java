a3395ad30e5c5e2e48c4db63fdc60605c27c4dd0
Adityaraj62 <118107897+Adityaraj62@users.noreply.github.com>
2022-11-17 16:30:05 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,25 @@
+import java.util.*; 
+class Add
+{ 
+  
+  public static void main(String[]args)
+   
+   {
+  
+     Scanner sc= new Scanner(System.in);
+ 
+       System.out.println("enter the first no.");
+  
+           int a=sc.nextInt();
+   
+       System.out.println("enter the second no.");
+   
+           int b=sc.nextInt();
+   
+           int c=a+b;
+    
+        System.out.println("output is"+c);
+
+   }
+
+}
